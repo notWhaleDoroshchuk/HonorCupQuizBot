4e2898ff1de3b49c93ac5c47b3101c1db21c7987
Nikita <n.doroshch@mail.ru>
2020-11-08 16:46:02 +0700

    born of bot for huawi battlecup

---
+++ main.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+TOKEN =  'https://quiz.honorcup.ru/app/?id=57684&sign=3112e1502ca3ade40a8519c05df7c06d'
+from selenium import webdriver
+import time
+browser = webdriver.Firefox(executable_path='geckodriver.exe')
+browser.get(TOKEN)
+battle_button = browser.find_element_by_class_name('about__buttons')
+print(battle_button)
+battle_button.click()
+time.sleep(4)
+category = browser.find_elements_by_class_name('slider__item')
+category[1].click()
+theme = browser.find_elements_by_class_name('profile__theme')
+theme[1].click()
+game = browser.find_element_by_xpath('/html/body/app/div[1]/nomination/div/div/div[2]/div[3]/div[2]/div/div/div[2]/div')#('button-group-2x')
+game.click();
+while 1:
+    while 1:
+        if browser.find_elements_by_class_name('game__answer') :
+            print("true")
+            break
